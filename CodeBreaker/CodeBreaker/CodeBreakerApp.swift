33b319ad177723342bcad6fe7df8654ab6edde7d
HAL24 <struligt@gmail.com>
2026-06-14 15:01:00 +0200

    new project

---
+++ CodeBreaker/CodeBreaker/CodeBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreakerApp.swift
+//  CodeBreaker
+//
+//  Created by danielringskog on 6/14/26.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodeBreaker/CodeBreaker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CodeBreaker
+//
+//  Created by danielringskog on 6/14/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
