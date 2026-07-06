e569ee07f5759581425c65866e2702634a2f1156
HAL24 <struligt@gmail.com>
2026-07-06 12:47:37 +0200

    Move UI extensions to own file

--- CodeBreaker/CodeBreaker/UI/CodeBreakerView.swift
+++ CodeBreaker/CodeBreaker/UI/CodeBreakerView.swift
@@ -79,35 +79,6 @@ struct CodeBreakerView: View {
     }
 }
 
-extension Animation {
-    static let codeBreaker = Animation.easeOut(duration: 3)
-    static let guess = Animation.codeBreaker
-    static let restart = Animation.codeBreaker
-}
-
-extension AnyTransition {
-    static let pegChooser = AnyTransition.offset(x: 0, y:200)
-    static func attempt(_ isOver: Bool)-> AnyTransition {
-        return AnyTransition.asymmetric(
-            insertion: isOver ? .opacity : .move(edge:.top),
-            removal: .move(edge:.trailing))
-    }
-}
-
-extension View {
-    func flexibleSystemFont(minimum: CGFloat = 8, maximum:CGFloat = 80) -> some View {
-        self
-            .font(.system(size: maximum)) // GuessButton.maxFontSize))
-            .minimumScaleFactor(minimum/maximum)// (GuessButton.scaleFactor)
-    }
-}
-
-extension Color  {
-    static func gray(_ brightness: CGFloat) -> Color {
-        return Color(hue: 148/360, saturation: 0, brightness: brightness)
-    }
-}
-
 
 #Preview {
     CodeBreakerView()
