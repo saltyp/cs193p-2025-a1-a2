833f2b1088fd9270dc0aa01eefd79c69d6e11dca
HAL24 <struligt@gmail.com>
2026-07-15 10:24:37 +0200

    Add as example secctions in list

--- CodeBreaker/CodeBreaker/UI/GameChooser.swift
+++ CodeBreaker/CodeBreaker/UI/GameChooser.swift
@@ -12,10 +12,18 @@ struct GameChooser: View {
     @State private var games : [CodeBreaker] = []
     
     var body: some View {
-        List(games, id: \.pegChoices) { game in
-            GameSummary(game:game)
-         }
-        .listStyle(.plain)
+        List {
+            Section("Games") {
+                ForEach(games, id: \.pegChoices) {
+                    game in GameSummary(game:game)
+                }
+            }
+            Section(header: Image(systemName: "face.smiling").font(.title)) {
+                Text("hello")
+                Text("there")
+            }
+        }
+//        .listStyle(.plain)
         .onAppear { //following is list of action that appears before View appears:
             games.append(CodeBreaker(name : "Mastermind", pegChoices: [.red, .blue, .yellow, .green]))
             games.append(CodeBreaker(name : "Earth Tones", pegChoices: [.orange, .brown, .black, .yellow, .green]))
