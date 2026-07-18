f193debb675a1958908ca3cc7576508db6b37204
HAL24 <struligt@gmail.com>
2026-07-18 15:23:22 +0200

    Alert and disable using alerts , not disabling Done button, in case of invalide game created.

--- CodeBreaker/CodeBreaker/UI/GameEditor.swift
+++ CodeBreaker/CodeBreaker/UI/GameEditor.swift
@@ -8,18 +8,28 @@
 import SwiftUI
 
 struct GameEditor: View {
+    //MARK: Data (Function) In
     @Environment(\.dismiss) var dismiss
 
+    //MARK: Data Shared with Me
     @Bindable var game: CodeBreaker //@Bind*able* allows referencing the @Observable and thereby using $game
     
     //MARK: Action Function
     let onChoose: () -> Void
+    
+    //MARK: Data Owned by Me
+    @State private var showInvalidGameAlert = false
     
     var body: some View {
         NavigationStack {
             Form {
                 Section("Name") {
                     TextField("Name", text: $game.name)
+                        .onSubmit {
+                            done()
+                        }
+                        .autocapitalization(.words)
+                        .autocorrectionDisabled(true)
                 }
                 Section("Pegs") {
                     PegChoicesChooser(pegChoices: $game.pegChoices)
@@ -33,12 +43,34 @@ struct GameEditor: View {
                     }
                     ToolbarItem(placement:.confirmationAction) {
                         Button("Done") {
-                            onChoose()
-                            dismiss()
+                            done()
+                        }
+//                        .disabled(!game.isValid)
+                        .alert("Invalid Game", isPresented: $showInvalidGameAlert) {
+                            Button("OK") {
+                                showInvalidGameAlert = false
+                            }
+                        } message: {
+                            Text("A game must have a name and more than one unique peg.")
                         }
                     }
                 }
         }
+    }
+    
+    func done() {
+        if game.isValid {
+            onChoose()
+            dismiss()
+        } else {
+            showInvalidGameAlert = true
+        }
+    }
+}
+
+extension CodeBreaker {
+    var isValid: Bool {
+        !name.isEmpty && Set(pegChoices).count >= 2
     }
 }
 
